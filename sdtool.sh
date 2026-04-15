263fbdf1f8031b9fea39bbf1b75449447787cd68
htkitty <eulfs@proton.me>
2026-04-15 15:08:06 +0200

    Update sdtool.sh

--- sdtool.sh
+++ sdtool.sh
@@ -1,15 +1,21 @@
-#url flag
+#config flags
 url="http://localhost:8080"
+tor=1
+
+#torconf
+if [[ tor == 1 ]] then
+	opt="torify"; tor &
+fi
 
 #logic
 if !command -v wget > /dev/null; then
 	printf "cannot find wget, trying curl\n"
 	while true; do
-		curl -I "$url"
+		$opt curl -I "$url"
 	done
 fi
 if command -v wget > /dev/null; then
 	while true; do
-		wget "$url"
+		$opt wget "$url"
 	done
 fi
