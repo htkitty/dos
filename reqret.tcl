5e76c47e289be868351b2495dc15881dab24dc70
htkitty <eulfs@proton.me>
2026-04-16 22:39:32 +0200

    req ret

--- reqret.tcl
+++ reqret.tcl
@@ -9,10 +9,13 @@ set ptl [lindex $argv 1]
 set portsc [socket $host $ptl]
 
 fconfigure $portsc -buffering line
-
+if {$ptl == "80"} {
 puts $portsc "GET / HTTP/1.1\nHost: $host\nConnection: close\n\n"
 
 while {[gets $portsc resp] >= 0} {
        puts $resp
 }
 close $portsc
+} else {
+	puts "Non-supported protocol number;"
+}
